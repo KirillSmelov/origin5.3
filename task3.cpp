e006644eeb82dd67b40590964c533f93d2d4f8f7
Kirill <1altumsilentium1@gmail.com>
2023-03-24 21:31:34 +0300

    Fixed

--- task3.cpp
+++ task3.cpp
@@ -20,21 +20,24 @@ public:
 	{
 
 	}
+	virtual std::string GetName()
+	{
+		return name;
+	}
 	virtual bool Check()
 	{
 		return true;
 	}
-	bool correct = Check();
 	Figure(int sum)
 	{
 		this->sum = sum;
 	}
 	virtual void print()
 	{
-		std::cout << name <<":" << std::endl;
-		if (correct)
+		std::cout << GetName() <<":" << std::endl;
+		if (Check()==true)
 		{
-			std::cout << "Правильная"<<std::endl;
+			std::cout << "Правильная" << std::endl;
 		}
 		else std::cout << "Неправильная"<<std::endl;
 		std::cout << "Колличество сторон: " << sum << std::endl;
@@ -49,16 +52,15 @@ protected:
 	int sum;
 	std::string name = "Треугольник";
 public:
-	Triangle(int sum, int a, int b, int c, int A, int B, int C) :Figure(sum)
+	Triangle( int a, int b, int c, int A, int B, int C) :Figure(sum)
 	{
-		this->sum = sum;
 		this->a = a;
 		this->b = b;
 		this->c = c;
 		this->A = A;
 		this->B = B;
 		this->C = C;
-
+		sum = 3;
 	}
 	void NumSides() override
 	{
@@ -72,28 +74,24 @@ public:
 	{
 		std::cout << "Углы: " << "A = " << A << " B = " << B << " C = " << C << std::endl;
 	}
-	int* ammount = &sum;
-	int* FirstAngle = &A;
-	int* SecondAngle = &B;
-	int* ThirdAngle = &C;
+	 std::string GetName() override
+	{
+		return name;
+	}
 	bool Check() override
 	{
-		if ((*ammount < 3 || *ammount>3) || (*FirstAngle + *SecondAngle + *ThirdAngle != 180))
+		if ((A + B + C) != 180)
 		{
 			return false;
 		}
-		else 
-		{
-			 return true;
-		}
+		else return true;
 	}
-	bool correct = Check();
 	
 	
 	void print() override
 	{
-		std::cout << name << ":" << std::endl;
-		if (correct == true)
+		std::cout << GetName() << ":" << std::endl;
+		if (Check() == true)
 		{
 			std::cout << "Правильная" << std::endl;
 		}
@@ -103,13 +101,257 @@ public:
 		Angles();
 	}
 };
+class Triangle90 :public Triangle
+{
+protected:
+	std::string name = "Прямоугольный треугольник";
+	int a, b, c;
+	int A, B, C;
+public:
+	Triangle90(int a, int b, int c, int A, int B, int C) :Triangle(a, b, c, A, B, C)
+	{
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+	}
+	 std::string GetName() override
+	{
+		return name;
+	}
+	bool Check() override
+	{
+		if ((A + B + C) != 180 || C != 90) return false;
+		else return true;
+	}
+};
+class Isoscelestriangle :public Triangle
+{
+protected:
+	std::string name = "Равнобедренный треугольник";
+	int a, b, c;
+	int A, B, C;
+public:
+	Isoscelestriangle(int a, int b, int c, int A, int B, int C) :Triangle(a, b, c, A, B, C)
+	{
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+	}
+	std::string GetName() override
+	{
+		return name;
+	}
+	bool Check() override
+	{
+		if ((A + B + C) != 180 || (a != c && A != C))
+		{
+			return false;
+		}
+		else return true;
+	}
+	
+};
+class Triangle60 :public Triangle
+{
+protected:
+	std::string name = "Равносторонний треугольник";
+	int a, b, c, A, B, C;
+public:
+	Triangle60(int a, int b, int c, int A, int B, int C) :Triangle(a, b, c, A, B, C)
+	{
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+	}
+	std::string GetName() override
+	{
+		return name;
+	}
+	bool Check() override
+	{
+		if ((A + B + C != 180) || (a != b || b != c || a != c || A != B || A != C || B != C)) return false;
+		else return true;
+	}
+};
 class Quadrangle :public Figure
 {
+protected:
+	int a, b, c, d;
+	int A, B, C, D;
+	std::string name = "Четырёхугольник";
+	int sum = 4;
+public:
+	Quadrangle(int a, int b, int c, int d, int A, int B, int C, int D) :Figure(a)
+	{
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->d = d;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+		this->D = D;
+	}
+	 std::string GetName() override
+	{
+		return name;
+	}
+	 bool Check() override
+	 {
+		 if (A + B + C + D != 360) return false;
+		 else return true;
+	 }
+	 void NumSides() override
+	 {
+		 std::cout << "Колличество сторон: " << sum << std::endl;
+	 }
+	 void Sides() override
+	 {
+		 std::cout << "Стороны: " << "a = " << a << " b = " << b << " c = " << c <<" d = "<<d<< std::endl;
+	 }
+	 void Angles() override
+	 {
+		 std::cout << "Углы: " << "A = " << A << " B = " << B << " C = " << C <<" D = "<<D<< std::endl;
+	 }
+	 void print() override
+	 {
+		 std::cout << GetName() << ":" << std::endl;
+		 if (Check() == true)
+		 {
+			 std::cout << "Правильная" << std::endl;
+		 }
+		 else std::cout << "Неправильная" << std::endl;
+		 std::cout << "Колличество сторон: " << sum << std::endl;
+		 Sides();
+		 Angles();
+	 }
+};
+class Recangle :public Quadrangle
+{
+protected:
+	int a, b, c, d;
+	int A, B, C, D;
+	std::string name = "Прямоугольник";
+public:
+	Recangle(int a, int b, int c, int d, int A, int B, int C, int D) :Quadrangle(a,b,c,d,A,B,C,D)
+	{
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->d = d;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+		this->D = D;
+	}
+	std::string GetName() override
+	{
+		return name;
+	}
+	bool Check() override
+	{
+		if (A + B + C + D != 360||a!=c||b!=d||A!=90||B!=90||C!=90||D!=90) return false;
+		else return true;
+	}
 
+};
+class Square :public Quadrangle
+{
+protected:
+	int a, b, c, d;
+	int A, B, C, D;
+	std::string name = "Квадрат";
+public:
+	Square(int a, int b, int c, int d, int A, int B, int C, int D) :Quadrangle(a, b, c, d, A, B, C, D)
+	{
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->d = d;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+		this->D = D;
+	}
+	std::string GetName() override
+	{
+		return name;
+	}
+	bool Check() override
+	{
+		if (A + B + C + D != 360 || a != c ||a!=b||a!=d||b!=c|| b != d ||c!=d|| A != 90 || B != 90 || C != 90 || D != 90) return false;
+		else return true;
+	}
+};
+class Paralelogramm : public Quadrangle
+{
+protected:
+	int a, b, c, d;
+	int A, B, C, D;
+	std::string name = "Параллелограмм";
+public:
+	Paralelogramm(int a, int b, int c, int d, int A, int B, int C, int D) :Quadrangle(a, b, c, d, A, B, C, D)
+	{
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->d = d;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+		this->D = D;
+	}
+	std::string GetName() override
+	{
+		return name;
+	}
+	bool Check() override
+	{
+		if (A + B + C + D != 360 || a !=c||b!=d || A!=C||B!=D) return false;
+		else return true;
+	}
+};
+class Rhomb :public Quadrangle
+{
+protected:
+	int a, b, c, d;
+	int A, B, C, D;
+	std::string name = "Ромб";
+public:
+	Rhomb(int a, int b, int c, int d, int A, int B, int C, int D) :Quadrangle(a, b, c, d, A, B, C, D)
+	{
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->d = d;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+		this->D = D;
+	}
+	std::string GetName() override
+	{
+		return name;
+	}
+	bool Check() override
+	{
+		if (A + B + C + D != 360 || a != c || a != b || a != d || b != c || b != d || c != d ||  A!=C ||B!=D) return false;
+		else return true;
+	}
 };
 void print(Figure* figure)
 {
 	figure->print();
+	std::cout << std::endl;
 }
 
 
@@ -119,10 +361,24 @@ int main(int argc, char** argv)
 	system("chcp 1251");
 	Figure figure(0);
 	print(&figure);
-	Triangle triangle(3, 10, 20, 30, 50, 60, 70);
+	Triangle triangle( 10, 20, 30, 50, 60, 70);
 	print(&triangle);
-
-	
+	Triangle90 triangle90(10, 20, 30, 50, 60, 90);
+	print(&triangle90);
+	Isoscelestriangle isoscelestriangle(10, 20, 10, 50, 80, 50);
+	print(&isoscelestriangle);
+	Triangle60 triangle60(30, 30, 30, 60, 60, 60);
+	print(&triangle60);
+	Quadrangle quadrangle(10, 20, 30, 40, 50, 60, 70, 80);
+	print(&quadrangle);
+	Recangle rectangle(10, 20, 10, 20, 90, 90, 90, 90);
+	print(&rectangle);
+	Square square(20, 20, 20, 20, 90, 90, 90, 90);
+	print(&square);
+	Paralelogramm paralelogramm(20, 30, 20, 30, 30, 40, 30, 40);
+	print(&paralelogramm);
+	Rhomb rhomb(30, 30, 30, 30, 30, 40, 30, 40);
+	print(&rhomb);
 	return 0;
 }
 
